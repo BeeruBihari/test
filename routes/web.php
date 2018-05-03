c1d8660cf4125b4da1e0f328300adde3253726f4
im-noob <aaravonly4you@gmail.com>
2018-05-03 21:52:39 +0530

    message

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,17 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Including web Routes
+|--------------------------------------------------------------------------
+|
+| Here is the list of all route that are required to start the app. If the
+| any of the given route is missing apllication will not run until route
+| is put into the "routes" directory.
+|
+*/
 
+
+include_once(__DIR__."/AccessLevel.php");
 /*
 |--------------------------------------------------------------------------
 | Web Routes
